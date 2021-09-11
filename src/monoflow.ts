77e530e77cc3e074090b8337666e9dbc8ba9a697
Brian Mock <brian@mockbrian.com>
2021-09-10 19:00:45 -0700

    better?

--- src/monoflow.ts
+++ src/monoflow.ts
@@ -1,76 +1,74 @@
-interface Hmm<T> {
-  then<F>(fn: (item: T) => F): Hmm<F>;
-  else<F>(fn: (error: Error) => F): Hmm<F>;
-  unwrap(fallback?: (error: Error) => T): T;
-}
+/**
+ * A workflow starts with a `TInit` value, which is supplied at the end via
+ * `.run`. Each `Workflow` is only a single step, linked together as a linked
+ * list, where `TIn` and `TOut` represents the input and output types of the
+ * handler function for that step. The handlers supplied to `else` work like the
+ * `catch` in `try/catch`: the workflow will skip all intermediate steps until
+ * it finds an `else` handler, and throw an exception if it doesn't find one.
+ */
+class Workflow<TInit, TIn, TOut> {
+  static create<TIn, TOut>(ok: (a: TIn) => TOut): Workflow<TIn, TIn, TOut> {
+    return new Workflow<TIn, TIn, TOut>(ok);
+  }
 
-class Yep<T> implements Hmm<T> {
-  constructor(private readonly _item: T) {}
+  private constructor(
+    private readonly _ok: ((a: TIn) => TOut) | undefined = undefined,
+    private readonly _err: ((error: Error) => TOut) | undefined = undefined,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    private readonly _next: Workflow<any, any, any> | undefined = undefined
+  ) {}
 
-  then<F>(fn: (item: T) => F): Hmm<F> {
-    try {
-      return new Yep(fn(this._item));
-    } catch (err) {
-      return new Nope(err as Error);
+  then<TOut2>(fn: (item: TOut) => TOut2): Workflow<TInit, TOut, TOut2> {
+    return new Workflow(fn, undefined, this);
+  }
+
+  else<TOut2>(fn: (error: Error) => TOut2): Workflow<TInit, TOut, TOut2> {
+    return new Workflow(undefined, fn, this);
+  }
+
+  run(value: TInit): TOut {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const steps: Workflow<any, any, any>[] = [this];
+    let step = this._next;
+    while (step) {
+      steps.unshift(step);
+      step = step._next;
     }
-  }
-
-  else<F>(_fn: (error: Error) => F): Hmm<F> {
-    return this as unknown as Yep<F>;
-  }
-
-  unwrap(_fallback?: (error: Error) => T): T {
-    return this._item;
-  }
-}
-
-class Nope<T> implements Hmm<T> {
-  constructor(public readonly err: Error) {}
-
-  then<F>(_fn: (item: T) => F): Hmm<F> {
-    return this as unknown as Nope<F>;
-  }
-
-  else<F>(fn: (error: Error) => F): Hmm<F> {
-    try {
-      return new Yep(fn(this.err));
-    } catch (err) {
-      return new Nope(err as Error);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let ret: any = value;
+    let i = 0;
+    while (i < steps.length) {
+      let step = steps[i];
+      try {
+        if (step._ok) {
+          ret = step._ok(ret);
+        }
+      } catch (err) {
+        while (step && !step._err) {
+          i++;
+          step = steps[i];
+        }
+        if (step && step._err) {
+          ret = step._err(err as Error);
+        } else {
+          throw err;
+        }
+      }
+      i++;
     }
-  }
-
-  unwrap(_fallback?: (error: Error) => T) {
-    if (_fallback) {
-      return _fallback(this.err);
-    }
-    throw this.err;
+    return ret;
   }
 }
 
-function log<T>(item: T): T {
-  console.log(item);
-  return item;
-}
-
-// I'm trying to figure out how to move the `3` to the end like `.run(3)`...
-const value = new Yep(3)
-  .then((x) => x * 10)
-  .then(log)
-  .then(() => {
-    throw new Error("oh no");
-  })
-  .else((err) => {
-    console.error("recovered error:", err.message);
-    return 42;
-  })
-  .then(log)
-  .then(() => {
-    throw new Error("not again");
-  })
-  .else((err) => {
-    console.error("recovered error:", err.message);
-    return 200;
-  })
-  .unwrap();
+// Argument type MUST be annotated on the first function
+const value = Workflow.create((x: number) => x * 10)
+  .then((x) => x + 2)
+  .then((x) => `cool ${x}`)
+  // .then((_x) => {
+  //   throw new Error("oops");
+  // })
+  .else((_err) => "NICE")
+  .then((x) => x)
+  .run(3);
 
 console.log(value);
